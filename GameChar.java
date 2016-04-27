5ce3012c40b7720e2e9e8aaaed378468f9563f52
purplconroy <willowindevilslayer@gmail.com>
2016-04-27 10:17:52 -0700

    Create GameChar.java

---
+++ GameChar.java
@@ -0,0 +1,16 @@
+public class Gamechar{
+  private int maxHealth;
+  private GameObject[] inventory;
+  private boolean dropInv;
+  public Gamechar(int h, GameObject[] inv, boolean f){
+    maxHealth = h;
+    inventory = inv;
+    dropInv = f;
+  }
+  public void die(){
+  
+  }
+  public GameObject drop(){
+  
+  }
+}
