560f1eab4f825a51dda93db8043b5ac35902b256
Anubhav <anubhav@egarg.com>
2016-04-29 09:20:15 -0700

    GameMob

--- src/GameMob.java
+++ src/GameMob.java
@@ -1,9 +1,26 @@
-import Items.GameObject;
-
 public class GameMob {
 
 	int maxHealth;
 	int health;
 	GameObject[] dropinventory;
+
+	public GameMob(int myHealth) {
+		maxHealth = myHealth;
+		health = maxHealth;
+	}
+
+	public void setDrops(GameObject[] objs) {
+		dropinventory = objs;
+	}
+
+	public void addDrop(GameObject obj) {
+		// dropinventory.add(obj)
+	}
+
+	public void takeDamage(int damage) {
+		health -= damage;
+		if (health <= 0)
+		// kill (maybe use an interface to do so)
+	}
 
 }
