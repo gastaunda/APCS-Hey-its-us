ec192420863f353f30dc85ed46b9da7ece6f3c55
Ninjamarth31 <tweeterfriend@gmail.com>
2016-05-02 09:06:17 -0700

    Update GameObject.java

--- src/Items/GameObject.java
+++ src/Items/GameObject.java
@@ -1,4 +1,21 @@
 package Items;
 public class GameObject{
 	private boolean drops;
+	private String name;
+	private String description;
+	
+	public GameObject(String newname, String newdescription)
+	{
+		name = newname;
+		description = newdescription;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	
+	
+	
 }
