0816ec5e141fb4478cc6ab14e85e6050e1983331
Garrison Astaunda <gastaunda@gmail.com>
2016-05-06 09:20:26 -0700

    created array of sprites

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,9 @@
+
+import java.awt.image.BufferedImage;
 import Items.GameEntity;
 
 public class Game {
 	private Block[][] map;
+	private BufferedImage[] sprites;
 	private GameEntity[] entities;
 }
