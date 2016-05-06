1fa9ddb7e132a7449e9d4e4bddc5b10d7d398c6f
Garrison Astaunda <gastaunda@gmail.com>
2016-05-06 08:54:18 -0700

    made Timer more readable

--- src/Timer.java
+++ src/Timer.java
@@ -1,12 +1,11 @@
-import java.util.*;
 
 //This is a timer for things that require time (i.e. position based on velocity/accel; projectile life (time based); jump time, ect)
-public class timer 
+public class Timer 
 {
 	private int time;
 	private long t1;
 	
-	public timer(int t)
+	public Timer(int t)
 	{
 		time = t * 1000;
 	}
@@ -14,15 +13,13 @@ public class timer
 	public boolean countdown()
 	{
 		t1 = System.currentTimeMillis();
-        int b = 5;
-        while (b > 0)
+        while (true)
         {
 
             int a = (int) (System.currentTimeMillis() - t1);
-            if (a >= t)
+            if (a >= time)
             {
-                b = -10;
-                System.out.println("Time extra passed(int millis): " + (a % t)); //(This is to test efficiency when implemented)
+                System.out.println("Time extra passed(int millis): " + (a % time)); //(This is to test efficiency when implemented)
                 //If method is too inefficent, then we'll have to either find a better 'timer', or copy/paste getElapsedTime() (i have standalone version on blueJay) into method directly)
                 return true;
                 //OR return a / 1000;
