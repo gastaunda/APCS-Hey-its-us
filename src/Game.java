a89344ee2c3a542cb0c58aa0c37f32a23e9491e7
Garrison Astaunda <gastaunda@gmail.com>
2016-05-10 09:17:18 -0700

    Modified Libraries

--- src/Game.java
+++ src/Game.java
@@ -1,23 +1,9 @@
-import java.io.IOException;
-
-import org.newdawn.easyogg.OggClip;
 
 public class Game {
-	private OggClip music;
 
 	public static void main(String[] args) {
-		Game game = new Game("assets\\music\\There is no game.ogg");
-		while (!game.music.stopped())
-			;
 	}
 
 	public Game(String path) {
-		try {
-			music = new OggClip(path);
-			music.play();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 }
