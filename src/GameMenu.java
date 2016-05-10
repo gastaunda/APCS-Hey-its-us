f5ebbad674383baba5b59e260615c71c559d6635
Ninjamarth31 <tweeterfriend@gmail.com>
2016-05-10 08:52:30 -0700

    More changes
    Added a game menu

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,4 +1,22 @@
 
-public class GameMenu {
-
+public class GameMenu 
+{
+	
+	String image;
+	private String Button1;
+	private String Button2;
+	private String Button3;
+	private String Button4;
+	private String Button5;
+	
+	public GameMenu(String img)
+	{
+		image = img;
+		Button1 = "Play";
+		Button2 = "How To Play";
+		Button3 = "Options";
+		Button4 = "Achievements";
+		Button5 = "Exit";
+		
+	}
 }
