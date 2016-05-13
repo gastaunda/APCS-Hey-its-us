978b2c1e51a04551607c638d060f03d2b7267f4f
Garrison Astaunda <gastaunda@gmail.com>
2016-05-13 08:41:17 -0700

    Menu

--- src/Game.java
+++ src/Game.java
@@ -3,17 +3,14 @@ public class Game
 {
 
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
 		Music test = new Music("There is no game.ogg");
 		test.play();
-=======
 		Game game = new Game();
->>>>>>> Stashed changes
 	}
 
 	public Game() {
-		GameMenu gm = new GameMenu("img.gif");
-		Music m = new Music();
+		GameMenu gm = new GameMenu();
+		Music m = new Music("");
 	}
 
 }
--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,17 +1,21 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JPanel;
 import javax.swing.JDesktopPane;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.UIManager;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 public class GameMenu {
 
-	private JFrame frame;
+	private JFrame frmMainMenu;
+	private final Action action = new SwingAction();
 
 	/**
 	 * Launch the application.
@@ -21,7 +25,7 @@ public class GameMenu {
 			public void run() {
 				try {
 					GameMenu window = new GameMenu();
-					window.frame.setVisible(true);
+					window.frmMainMenu.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -40,15 +44,50 @@ public class GameMenu {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		frmMainMenu = new JFrame();
+		frmMainMenu.getContentPane().setForeground(Color.BLACK);
+		frmMainMenu.setTitle("Main Menu");
+		frmMainMenu.setBounds(100, 100, 450, 300);
+		frmMainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		JDesktopPane desktopPane = new JDesktopPane();
-		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(173, 10, 88, 23);
+		frmMainMenu.getContentPane().add(desktopPane, BorderLayout.CENTER);
+
+		JButton btnNewButton = new JButton("Levels");
+		btnNewButton.setFont(UIManager.getFont("Button.font"));
+		btnNewButton.setBounds(172, 10, 90, 23);
 		desktopPane.add(btnNewButton);
+
+		JButton btnLevelPacks = new JButton("Level Packs");
+		btnLevelPacks.setFont(UIManager.getFont("Button.font"));
+		btnLevelPacks.setBounds(172, 44, 90, 23);
+		desktopPane.add(btnLevelPacks);
+
+		JButton btnOptions = new JButton("Options");
+		btnOptions.setFont(UIManager.getFont("Button.font"));
+		btnOptions.setBounds(172, 78, 90, 23);
+		desktopPane.add(btnOptions);
+
+		JButton btnExit = new JButton("Exit");
+		btnExit.setAction(action);
+		btnExit.setFont(UIManager.getFont("Button.font"));
+		btnExit.setBounds(172, 227, 90, 23);
+		desktopPane.add(btnExit);
+
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(GameMenu.class.getResource("/assets/images/LH_95.png")));
+		label.setLabelFor(frmMainMenu);
+		label.setBounds(0, 0, 434, 261);
+		desktopPane.add(label);
+	}
+
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Exit");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
