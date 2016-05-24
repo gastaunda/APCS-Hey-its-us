451c9a25ae7b80c999fa3047d7526d8d8df4e2da
Wally012 <anubhav@egarg.com>
2016-05-23 20:37:16 -0700

    small tweak

--- src/GUIPractice/GraphicExperiment.java
+++ src/GUIPractice/GraphicExperiment.java
@@ -37,7 +37,7 @@ public class GraphicExperiment extends JPanel implements ActionListener, KeyList
 		for (double i = 100; i <= 600; i += 100) {
 			lischt.add(new Wall(25, 300, i));
 		}
-		//lischt.add(new Wall(25,100, 150));
+		lischt.add(new Wall(25,100, 150));
 		for (double i = 350; i <= 600; i += 50){
 			lischt.add(new Floor(25, i, 500));
 		}
