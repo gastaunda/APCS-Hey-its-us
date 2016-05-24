4218abf6e1e02546d4bb67b4bb7b107455916cb4
Garrison Astaunda <gastaunda@gmail.com>
2016-05-24 09:19:32 -0700

    image stuff

--- src/items/KillBlock.java
+++ src/items/KillBlock.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 public class KillBlock extends Wall{
 	public KillBlock(double length, double xLoc, double yLoc){
 		super(length, xLoc, yLoc);
-		myImage = new ImageIcon("KillBrick.png");
+		myImage = new ImageIcon(KillBlock.class.getResource("/assets/images/KillBrick.png"));
 	}
 	public void collide(GameEntity other){
 			other.takedamage(999);
