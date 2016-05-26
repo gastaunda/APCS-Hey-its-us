e66b27486aebaca5dbc0fddb44f35976cb963da5
Anubhav <anubhav@egarg.com>
2016-05-26 08:20:06 -0700

    there waws a typo

--- src/items/Floor.java
+++ src/items/Floor.java
@@ -7,7 +7,7 @@ public class Floor extends Wall {
 	}
 	public void collide(GameEntity other){
 		double x = Math.random() * -4;
-		other.setVelY(x.);
+		other.setVelY(x);
 		other.setAccY(0.05);
 		other.setGround(true);
 	}
