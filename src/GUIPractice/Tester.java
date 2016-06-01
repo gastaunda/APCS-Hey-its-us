9d698bee852a379f37b7f90c33f4e058c46fa4b8
Garrison Astaunda <gastaunda@gmail.com>
2016-05-31 18:26:15 -0700

    Modified Music

--- src/GUIPractice/Tester.java
+++ src/GUIPractice/Tester.java
@@ -51,8 +51,9 @@ public class Tester {
 	private static void startmusic() {
 		if (Game.m != null)
 			Game.m.close();
-		Game.m = new oggVorbis.Music("assets/audio/music/If you don't know what to do.ogg",
-				"assets/audio/music/just steal the amen break.ogg");
+		Game.m = new oggVorbis.Music("assets/audio/music/If you don't know what to do just steal the amen break 1.ogg",
+				"assets/audio/music/If you don't know what to do just steal the amen break 2.ogg",
+				"assets/audio/music/If you don't know what to do just steal the amen break 3.ogg");
 		Game.m.loop();
 
 	}
